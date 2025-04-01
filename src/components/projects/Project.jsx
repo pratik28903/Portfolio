edf2ae8f57a9748d56bfe42d32eb99441471f4d9
Pratik Ostwal <75239298+pratik28903@users.noreply.github.com>
2025-04-01 11:57:46 +0530

    Update Project.jsx

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -103,14 +103,14 @@ const Project = () => {
   </p>
 
 <div style={{ display: "flex", gap: "10px", flexWrap: "wrap" }}>
-  <a href={ManageTicket} target="_blank" rel="noopener noreferrer">
-    <img src={ManageTicket} alt="Manage Ticket" style={{ width: '100px', borderRadius: '8px' }} />
+    <a href={Dashboard} target="_blank" rel="noopener noreferrer">
+    <img src={Dashboard} alt="Dashboard" style={{ width: '100px', borderRadius: '8px' }} />
   </a>
-  <a href={Manageuser} target="_blank" rel="noopener noreferrer">
+    <a href={Manageuser} target="_blank" rel="noopener noreferrer">
     <img src={Manageuser} alt="Manage User" style={{ width: '100px', borderRadius: '8px' }} />
   </a>
-  <a href={Dashboard} target="_blank" rel="noopener noreferrer">
-    <img src={Dashboard} alt="Dashboard" style={{ width: '100px', borderRadius: '8px' }} />
+  <a href={ManageTicket} target="_blank" rel="noopener noreferrer">
+    <img src={ManageTicket} alt="Manage Ticket" style={{ width: '100px', borderRadius: '8px' }} />
   </a>
 </div>
 
